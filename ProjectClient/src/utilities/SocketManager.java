8ac8730080e2e7366c1d5e70c14ff229b85921ed
Damiano <Damiano@DESKTOP-69TELN2.wind3.hub>
2024-10-10 14:21:38 +0200

    socketmanager

---
+++ ProjectClient/src/utilities/SocketManager.java
@@ -0,0 +1,56 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SocketManager {
+    private static SocketManager instance;
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    private SocketManager() {
+        try {
+            // Sostituisci con l'indirizzo e la porta del tuo server
+            this.socket = new Socket("localhost", 8080);
+            this.out = new PrintWriter(socket.getOutputStream(), true);
+            this.in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static synchronized SocketManager getInstance() {
+        if (instance == null) {
+            instance = new SocketManager();
+        }
+        return instance;
+    }
+
+    public PrintWriter getPrintWriter() {
+        return out;
+    }
+
+    public BufferedReader getBufferedReader() {
+        return in;
+    }
+
+    public void close() {
+        try {
+            if (socket != null) {
+                socket.close();
+            }
+            if (out != null) {
+                out.close();
+            }
+            if (in != null) {
+                in.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
